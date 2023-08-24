73304fce2ef693d9c3e80dfcb70f2eddf4985df9
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-08-24 13:40:26 +0800

    update

--- src/main/java/com/study/task/util/MsgUtils.java
+++ src/main/java/com/study/task/util/MsgUtils.java
@@ -113,7 +113,7 @@ public class MsgUtils {
                         reply = "用户已激活";
                     } else {
                         userToActivate.setHasQuited(false);
-                        userService.save(userToActivate);
+                        userService.updateById(user);
                         reply = "用户激活成功";
                     }
                 } else if (command.startsWith("delete ")) {
